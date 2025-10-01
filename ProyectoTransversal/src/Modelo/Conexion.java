64b56939cdae40b822395a383bc5869ee2d31551
Elias Pereyra <elias13pereyra@gmail.com>
2025-10-01 20:31:37 -0300

    Alumno creado

--- ProyectoTransversal/src/Modelo/Conexion.java
+++ ProyectoTransversal/src/Modelo/Conexion.java
@@ -4,10 +4,32 @@
  */
 package Modelo;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author elias
  */
 public class Conexion {
+    private static final String URL = "jdbc:mysql://localhost:3306/tp8";
+    private static final String USUARIO = "root";
+    private static final String CLAVE = ""; 
+
+    public static Connection conectar() {
+        Connection conexion = null;
+        try {
+            conexion = DriverManager.getConnection(URL, USUARIO, CLAVE);
+            System.out.println("✅ Conexión exitosa a la base de datos.");
+        } catch (SQLException e) {
+            System.out.println("❌ Error en la conexión: " + e.getMessage());
+        }
+        return conexion;
+    }
+
+
+        Connection c = Conexion.conectar();
     
 }
+
