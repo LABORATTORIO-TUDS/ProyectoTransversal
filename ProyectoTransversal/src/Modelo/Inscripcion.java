d6d54e85b9f0e7ed69ff53d7d96e3483a6b4316b
victormarianopelaez-glitch <victormarianopelaez@gmail.com>
2025-10-08 18:42:55 -0300

    inscripcion

---
+++ ProyectoTransversal/src/Modelo/Inscripcion.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Victor
+ */
+public class Inscripcion {
+    private int legajo;
+    private int nota;
+    private Alumno alumno;
+    private Materia materia;
+
+    public Inscripcion(int legajo, int nota, Alumno alumno, Materia materia) {
+        this.legajo = legajoInscripto;
+        this.nota = nota;
+        this.alumno = alumno;
+        this.materia = materia;
+    }
+
+    public Inscripcion(int nota, Alumno alumno, Materia materia) {
+        this.nota = nota;
+        this.alumno = alumno;
+        this.materia = materia;
+    }
+
+    public Inscripcion() {
+    }
+
+    public int getLegajo() {
+        return legajo;
+    }
+
+    public void setLegajo(int legajo) {
+        this.legajo = legajo;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+    
+
+  
+
+ 
+    
+}
